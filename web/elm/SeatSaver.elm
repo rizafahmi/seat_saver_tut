3f6f61aafa7522f1cea9294f66b8bf06133acc02
Riza Fahmi <rizafahmi@gmail.com>
2016-01-10 12:54:15 +0700

    Iterate through lists of seats
    http://www.cultivatehq.com/posts/phoenix-elm-4/

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -1,14 +1,45 @@
 module SeatSaver where
 
-import Html
+import Html exposing (Html, ul, li, text)
+import Html.Attributes exposing (class)
 
-main : Html.Html
+main : Html
 main =
-  view
+  view init
 
 
 -- VIEW
 
-view : Html.Html
-view =
-  Html.text "Wooohoo!! I'm in elm view!"
+view : Model -> Html
+view model =
+  ul [ class "seats" ] (List.map seatItem model)
+
+seatItem seat =
+  li [ class "seat available" ] [ text (toString seat.seatNo) ]
+
+
+-- MODEL
+
+type alias Seat =
+  { seatNo: Int
+  , occupied: Bool
+  }
+
+type alias Model =
+  List Seat
+
+init : Model
+init =
+  [ { seatNo = 1, occupied = False }
+  , { seatNo = 2, occupied = False }
+  , { seatNo = 3, occupied = False }
+  , { seatNo = 4, occupied = False }
+  , { seatNo = 5, occupied = False }
+  , { seatNo = 6, occupied = False }
+  , { seatNo = 7, occupied = False }
+  , { seatNo = 8, occupied = False }
+  , { seatNo = 9, occupied = False }
+  , { seatNo = 10, occupied = False }
+  , { seatNo = 11, occupied = False }
+  , { seatNo = 12, occupied = False }
+  ]
